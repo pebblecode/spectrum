5d6feaebdb6bf2d3fe7b16b3bdd16c9c518c6298
Tak Tran <contact@tutaktran.com>
2014-03-31 16:11:43 +0100

    Simple use of microphone.

---
+++ src/js/main.js
@@ -0,0 +1,19 @@
+(function (){
+  'use strict';
+
+  // success callback when requesting audio input stream
+  function gotStream(stream) {
+      window.AudioContext = window.AudioContext || window.webkitAudioContext;
+      var audioContext = new AudioContext();
+
+      // Create an AudioNode from the stream.
+      var mediaStreamSource = audioContext.createMediaStreamSource( stream );
+
+      // Connect it to the destination to hear yourself (or any other node for processing!)
+      mediaStreamSource.connect( audioContext.destination );
+  }
+
+  navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia;
+  navigator.getUserMedia( {audio:true}, gotStream );
+
+})();

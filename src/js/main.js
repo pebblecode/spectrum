db7e92948db8bec0666e405257598adc5ff4e731
Mark Durrant <mark.j.durrant.gmail.com>
2014-03-31 16:00:36 +0100

    add animation

--- src/js/main.js
+++ src/js/main.js
@@ -10,12 +10,29 @@ $(document).ready(function() {
       e = canvas.rect(0, 0, '020%', '100%').attr({fill: colors[4]});
 
   window.drawSA = drawSA;
+  window.randomSA = randomSA;
+  window.repeatRandom = repeatRandom;
 
   function drawSA( stop1, stop2, stop3, stop4 ) {
-    e.attr('width', stop1 + '%');
-    d.attr('width', stop2 + '%');
-    c.attr('width', stop3 + '%');
-    b.attr('width', stop4 + '%');
+    e.animate({'width': stop1 + '%'}, 100);
+    d.animate({'width': stop2 + '%'}, 100);
+    c.animate({'width': stop3 + '%'}, 100);
+    b.animate({'width': stop4 + '%'}, 100);
+  }
+
+  function randomSA () {
+    drawSA(
+      Math.floor(Math.random() * 20) + 10,
+      Math.floor(Math.random() * 20) + 30,
+      Math.floor(Math.random() * 20) + 50,
+      Math.floor(Math.random() * 20) + 70
+    );
+  }
+
+  function repeatRandom() {
+    setInterval( function() {
+      randomSA();
+    }, 100);
   }
 
 });
